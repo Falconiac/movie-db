7c9ff241f732a9207406b086010fc08912b8c25f
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-09-30 12:56:50 +0200

    FE - In App.tsx getAllMovies with adding to useble Array done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
 
+  const url = "/movie/";
 
+  let [movies, setMovies] = useState([]);
+
+  function getAllMovies(link:string){
+    axios.get(link)
+        .then((response)=>{
+          return response.data
+        }).then((data)=>{
+          setMovies(data)
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  useEffect(()=>{
+    getAllMovies(url);
+  },[])
 
   return (
     <section className="App">
