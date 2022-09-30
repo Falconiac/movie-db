74b61133123749c4774e0479d1c6024987c9430f
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-09-30 12:29:53 +0200

    BE- Controller - MovieController added

---
+++ backend/src/main/java/de/neuefische/backend/controller/MovieController.java
@@ -0,0 +1,29 @@
+package de.neuefische.backend.controller;
+
+
+import de.neuefische.backend.model.Movie;
+import de.neuefische.backend.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+
+    private MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        List<Movie> foundMovies = movieService.getAllMovies();
+        return foundMovies;
+
+    }}
