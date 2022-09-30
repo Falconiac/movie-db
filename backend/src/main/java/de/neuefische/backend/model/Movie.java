e95b4db5a33620547f75cf75fc9940abe926d976
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-09-30 11:33:13 +0200

    Movie-Model added

---
+++ backend/src/main/java/de/neuefische/backend/model/Movie.java
@@ -0,0 +1,16 @@
+package de.neuefische.backend.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    private String id ="";
+    private String title = "";
+    private String release ="";
+    private String poster = "";
+    private boolean isMarked = false;
+}
